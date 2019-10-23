e334f35d2c08437fbbdc6265c1c573290295af00
Mayur Patel <31582941+programmer-mayur@users.noreply.github.com>
2019-10-23 19:39:30 +0530

    Commit By Mayur

--- src/App.js
+++ src/App.js
@@ -8,16 +8,9 @@ function App() {
       <header className="App-header">
         <img src="/ctlogo.png" className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          CLOUD THAT
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
